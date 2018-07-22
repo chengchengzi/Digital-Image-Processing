575709125331e848dab099424695167cd96f536d
Orange <sueannika@163.com>
2018-07-22 11:18:02 +0800

    paddedsize & dftfilt

---
+++ CH03/dftfilt.m
@@ -0,0 +1,26 @@
+function g = dftfilt(f,H,classout)
+%   函数dftfilt接受输入图像，处理所有过滤细节，并输出滤波后的结果以及修剪图像
+%   classout的参数如下：
+%   'origin'    classout的默认值，输出图像与输入图像类型一样
+%   ’fltpoint' 输出图像是浮点类型，当f和H都是double类型时，输出图像也是double类型
+
+%   将输入图像变换为浮点类型
+[f,revertClass] = tofloat(f);
+
+%   获取填充输入的FFT
+F = fft2(f,size(H,1),size(H,2));
+
+%   进行滤波后反变换
+g = ifft2(H.*F);
+
+%   裁剪到原来的大小
+g = g(1:size(f,1),1:size(f,2));
+
+%   如果classout有值，根据变量的值转换输出图像
+if nargin == 2 || strcmp(classout,'origin')
+    g = revertClass(g);
+elseif strcmp(classout,'fltpoint')
+    return
+else
+    error('Undefined class for the output image.')
+end
---
+++ CH03/paddedsize.m
@@ -0,0 +1,28 @@
+function PQ = paddedsize(AB,CD,PARAM)
+%   函数paddedsize计算填充大小，适用于基于FFT的过滤器
+%   PQ = paddedsize(AB),计算PQ = 2*AB，AB是两个元素大小的向量
+%   
+%   PQ = paddedsize(AB,'PWR2') 计算 PQ(1) = PQ(2) = 2^nextpow(2*m),m是MAX（AB）
+%
+%   PQ = （AB,CD)，计算两个元素大小的PQ,PQ = AB + CD - 1
+%
+%   PQ = （AB,CD,'PWR2')计算向量PQ = PQ(1)=PQ(2)=2^nextpow2(2*m),m是MAX（[AB CD]）
+
+if nargin == 1
+    PQ = 2*AB;
+elseif nargin == 2 && ~ischar(CD)
+        PQ = AB + CD - 1;
+        PQ = 2 * ceil(PQ / 2);
+elseif nargin == 2
+        m = max(AB);
+        P = 2^nextpow(2*m);
+        PQ = [P,P];
+elseif (nargin == 3) && strctmpi(PARAM,'pwr2')
+    m = max([AB CD]);
+    p = 2^nextpow2(2*m);
+    PQ = [P,P];
+else
+    erroe('Wrong number of inputs.')
+end
+        
+        
